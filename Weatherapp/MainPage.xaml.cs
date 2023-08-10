c6a4fe07034a7258aa47c5e7b9c69ec8bf3d3d26
AnonimAnonim2245 <98339220+AnonimAnonim2245@users.noreply.github.com>
2023-08-10 13:15:01 +0300

    frf

--- Weatherapp/MainPage.xaml.cs
+++ Weatherapp/MainPage.xaml.cs
@@ -6,20 +6,6 @@ namespace Weatherapp;
 
 public partial class MainPage : ContentPage
 {
-    protected override void OnNavigatedTo(NavigatedToEventArgs args)
-    {
-        base.OnNavigatedTo(args);
-
-        var hanaLoc = new Location(20.7557, -155.9880);
-
-        MapSpan mapSpan = MapSpan.FromCenterAndRadius(hanaLoc, Distance.FromKilometers(3));
-        map.MoveToRegion(mapSpan);
-        map.Pins.Add(new Pin
-        {
-            Label = "Welcome to .NET MAUI!",
-            Location = hanaLoc,
-        });
-    }
     public MainPage(MainViewModel viewModel)
     {
         InitializeComponent();
